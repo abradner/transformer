9089734a4053cf59fdf7841559d39235a222e144
Alex Bradner <alex@bradner.net>
2025-07-19 18:56:34 +1000

    review feedback - adding tailwind as dep

--- app/javascript/controllers/transformation_controller.js
+++ app/javascript/controllers/transformation_controller.js
@@ -56,7 +56,7 @@ export default class extends Controller {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
+          'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]')?.content || ''
         },
         body: JSON.stringify({
           name: transformationName,
